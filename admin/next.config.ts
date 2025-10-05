5be92b96f95edc43de61eb5b69b63e3366b92bfc
Oğuzhan çilesiz <56759492+Oguzhancilesiz@users.noreply.github.com>
2025-10-06 00:28:07 +0300

    ts değişti yine

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,7 +1,7 @@
+// admin/next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Build’te ESLint’i ve TS hatalarını bloklamasın (demo/deploy için)
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
